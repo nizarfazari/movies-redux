cb1a4603dfa9e1efac7abeba3c61209ad8d73538
nizarfazari <nexsel32@gmail.com>
2022-11-04 20:20:07 +0700

    setup env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -39,13 +39,13 @@ const signInWithGoogle = async ({ loginHandle, getProfile }) => {
   }
 };
 const firebaseConfig = {
-  apiKey: "AIzaSyD3_Ra5n3dq6Akzys7O1Toe8X7JRpEjquQ",
-  authDomain: "authentication-39d0b.firebaseapp.com",
-  projectId: "authentication-39d0b",
-  storageBucket: "authentication-39d0b.appspot.com",
-  messagingSenderId: "784331731748",
-  appId: "1:784331731748:web:f535a048cd5c7e972bf9b1",
-  measurementId: "G-11CHPM748Z",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
